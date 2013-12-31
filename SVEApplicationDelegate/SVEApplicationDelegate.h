fa3366b5200150e8950106f7d2b4df9ea8269f97
Sérgio Estêvão <sergioestevao@Iness-MacBook-Air.local>
2013-12-31 16:07:02 +0000

    Initial Commit

---
+++ SVEApplicationDelegate/SVEApplicationDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SVEApplicationDelegate.h
+//  SVEApplicationDelegate
+//
+//  Created by Sérgio Estêvão on 31/12/2013.
+//  Copyright (c) 2013 Sérgio Estêvão. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVEApplicationDelegate : NSObject
+
+@end
---
+++ SVEApplicationDelegate/SVEApplicationDelegate.m
@@ -0,0 +1,13 @@
+//
+//  SVEApplicationDelegate.m
+//  SVEApplicationDelegate
+//
+//  Created by Sérgio Estêvão on 31/12/2013.
+//  Copyright (c) 2013 Sérgio Estêvão. All rights reserved.
+//
+
+#import "SVEApplicationDelegate.h"
+
+@implementation SVEApplicationDelegate
+
+@end
